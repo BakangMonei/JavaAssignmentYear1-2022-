90092cc79124a9c21221bc8d5cf55a2ef34d2cc2
BakangMonei <78691569+BakangMonei@users.noreply.github.com>
2022-06-01 19:03:05 +0200

    Update Interface.java

--- src/Interface.java
+++ src/Interface.java
@@ -17,15 +17,6 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
-
-/**
- *
- * @author Gosego
- */
 public class Interface extends javax.swing.JFrame {
 
     /**
